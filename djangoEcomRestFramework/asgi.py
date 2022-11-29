d0fa2f9f4d9f87a89fd378b6774f70d144fd62d3
renaldyhidayatt <renaldyhidayatt@gmail.com>
2022-11-30 01:31:15 +0700

    Initial:Commit

---
+++ djangoEcomRestFramework/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for djangoEcomRestFramework project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoEcomRestFramework.settings")
+
+application = get_asgi_application()
